99356909808370134abc84e496b91104e7ef979a
refractored <me@refractored.net>
2024-05-31 00:19:02 -0500

    refactor: Move gui's into seperate package

--- src/main/java/net/refractored/joblistings/commands/GetOrders.kt
+++ src/main/java/net/refractored/joblistings/commands/GetOrders.kt
@@ -5,6 +5,7 @@ import com.samjakob.spigui.item.ItemBuilder
 import com.samjakob.spigui.menu.SGMenu
 import net.refractored.joblistings.JobListings.Companion.spiGUI
 import net.refractored.joblistings.database.Database
+import net.refractored.joblistings.gui.AllOrders
 import net.refractored.joblistings.order.Order
 import net.refractored.joblistings.serializers.ItemstackSerializers
 import net.refractored.joblistings.util.MessageUtil
@@ -23,117 +24,8 @@ import kotlin.math.ceil
 class GetOrders {
     @CommandPermission("joblistings.order.create")
     @Description("Views order ingame")
-    @Command("joblistings orders")
-    fun getOrders(actor: BukkitCommandActor, @Optional page: Int = 1) {
-
-        val rows = 5
-
-        val gui = spiGUI.create("&c&lOrders &c(Page {currentPage}/{maxPage})", rows)
-
-        val pageCount = if (ceil(Database.orderDao.countOf().toDouble() / 21).toInt() > 0) {
-            ceil(Database.orderDao.countOf().toDouble() / 21).toInt()
-        } else {
-            1
-        }
-
-        val borderItems = listOf(
-            0,  1,  2,  3,  4,  5,  6,  7,  8,
-            9,                              17,
-            18,                             26,
-            27,                             35,
-                37, 38, 39, 40, 41, 42, 43,
-        )
-
-
-        for (i in 0..< pageCount) {
-            val pageSlot = 45 * i
-            borderItems.forEach{
-                gui.setButton(
-                    (it + pageSlot),
-                    SGButton(ItemBuilder(Material.GRAY_STAINED_GLASS_PANE)
-                    .name(" ")
-                    .build()
-                    ),
-                )
-            }
-        }
-
-        gui.setOnPageChange { inventory ->
-            if (gui.getButton(10 + (inventory.currentPage * 45)) == null) {
-                loadItems(inventory, actor)
-            }
-        }
-
-
-        actor.player.openInventory(gui.inventory)
-        loadItems(gui, actor)
+    @Command("joblistings orders all")
+    fun getOrders(actor: BukkitCommandActor) {
+        AllOrders.openAllOrders(actor)
     }
-
-    private fun loadItems(gui: SGMenu, actor: BukkitCommandActor){
-        gui.setButton(
-            ((gui.currentPage * 45) + 44),
-            SGButton(ItemBuilder(Material.ARROW)
-                .name("Next page")
-                .build()
-            ).withListener { event: InventoryClickEvent ->
-                gui.nextPage(actor.player)
-            },
-        )
-        gui.setButton(
-            ((gui.currentPage * 45) + 36),
-            SGButton(ItemBuilder(Material.ARROW)
-                .name("Previous page")
-                .build()
-            ).withListener { event: InventoryClickEvent ->
-                gui.previousPage(actor.player)
-            },
-        )
-        Order.getOrders(21, gui.currentPage * 21 ).forEachIndexed { index, order ->
-            val item = ItemstackSerializers.deserialize(order.item)!!.clone()
-            val itemMetaCopy = item.itemMeta
-            val infoLore = listOf(
-                MessageUtil.toComponent(""),
-                MessageUtil.toComponent("<reset><red>Pay: <white>${order.cost}"),
-                MessageUtil.toComponent("<reset><red>User: <white>${Bukkit.getOfflinePlayer(order.user).name}"),
-                MessageUtil.toComponent("<reset><red>Created: <white>${order.timeCreated}"),
-                MessageUtil.toComponent(""),
-                MessageUtil.toComponent("<reset><gray>(Click to accept order)"),
-            )
-
-            if (itemMetaCopy.hasLore()) {
-                val itemLore = itemMetaCopy.lore()!!
-                itemLore.addAll(infoLore)
-                itemMetaCopy.lore(itemLore)
-            } else {
-                itemMetaCopy.lore(infoLore)
-            }
-
-            item.itemMeta = itemMetaCopy
-
-            val button = SGButton(
-                item
-            ).withListener { event: InventoryClickEvent ->
-                event.whoClicked.sendMessage("Order Accepted (Broken!)")
-                event.whoClicked.closeInventory()
-                TODO()
-            }
-            val baseSlot = (index + 10) + (gui.currentPage * 45)
-
-            var slot = baseSlot
-
-            while (gui.getButton(slot) != null) {
-                if (slot >= 44) {
-                    throw IndexOutOfBoundsException("No more slots available in page ${gui.currentPage}")
-                }
-                slot++
-            }
-
-            gui.setButton( slot , button)
-
-
-        }
-        gui.refreshInventory(actor.player)
-
-    }
-
 }
--- src/main/java/net/refractored/joblistings/commands/ViewOrder.kt
+++ src/main/java/net/refractored/joblistings/commands/ViewOrder.kt
@@ -12,6 +12,7 @@ import revxrsal.commands.bukkit.BukkitCommandActor
 import revxrsal.commands.bukkit.annotation.CommandPermission
 import revxrsal.commands.bukkit.player
 import net.refractored.joblistings.JobListings.Companion.spiGUI
+import net.refractored.joblistings.gui.MyOrders
 import net.refractored.joblistings.order.Order
 import net.refractored.joblistings.util.MessageUtil
 import org.bukkit.Bukkit
@@ -22,119 +23,8 @@ import kotlin.math.ceil
 class ViewOrder {
     @CommandPermission("joblistings.order.view")
     @Description("View your order.")
-    @Command("joblistings view")
+    @Command("joblistings orders mine")
     fun ViewOrder(actor: BukkitCommandActor) {
-        val rows = 5
-
-        val gui = spiGUI.create("&c&lOrders &c(Page {currentPage}/{maxPage})", rows)
-
-        val pageCount = if (ceil(Database.orderDao.countOf().toDouble() / 21).toInt() > 0) {
-            ceil(Database.orderDao.countOf().toDouble() / 21).toInt()
-        } else {
-            1
-        }
-
-        val borderItems = listOf(
-            0,  1,  2,  3,  4,  5,  6,  7,  8,
-            9,                              17,
-            18,                             26,
-            27,                             35,
-            37, 38, 39, 40, 41, 42, 43,
-        )
-
-
-        for (i in 0..< pageCount) {
-            val pageSlot = 45 * i
-            borderItems.forEach{
-                gui.setButton(
-                    (it + pageSlot),
-                    SGButton(
-                        ItemBuilder(Material.GRAY_STAINED_GLASS_PANE)
-                        .name(" ")
-                        .build()
-                    ),
-                )
-            }
-        }
-
-        gui.setOnPageChange { inventory ->
-            if (gui.getButton(10 + (inventory.currentPage * 45)) == null) {
-                loadItems(inventory, actor)
-            }
-        }
-
-
-        actor.player.openInventory(gui.inventory)
-        loadItems(gui, actor)
-    }
-
-    private fun loadItems(gui: SGMenu, actor: BukkitCommandActor){
-        gui.setButton(
-            ((gui.currentPage * 45) + 44),
-            SGButton(
-                ItemBuilder(Material.ARROW)
-                .name("Next page")
-                .build()
-            ).withListener { event: InventoryClickEvent ->
-                gui.nextPage(actor.player)
-            },
-        )
-        gui.setButton(
-            ((gui.currentPage * 45) + 36),
-            SGButton(
-                ItemBuilder(Material.ARROW)
-                .name("Previous page")
-                .build()
-            ).withListener { event: InventoryClickEvent ->
-                gui.previousPage(actor.player)
-            },
-        )
-        Order.getPlayerCreatedOrders(21, gui.currentPage * 21, actor.uniqueId ).forEachIndexed { index, order ->
-            val item = ItemstackSerializers.deserialize(order.item)!!.clone()
-            val itemMetaCopy = item.itemMeta
-            val infoLore = listOf(
-                MessageUtil.toComponent(""),
-                MessageUtil.toComponent("<reset><red>Cost: <white>${order.cost}"),
-                MessageUtil.toComponent("<reset><red>User: <white>${Bukkit.getOfflinePlayer(order.user).name}"),
-                MessageUtil.toComponent("<reset><red>Created: <white>${order.timeCreated}"),
-                MessageUtil.toComponent(""),
-                MessageUtil.toComponent("<reset><gray>(Click to remove order)"),
-            )
-
-            if (itemMetaCopy.hasLore()) {
-                val itemLore = itemMetaCopy.lore()!!
-                itemLore.addAll(infoLore)
-                itemMetaCopy.lore(itemLore)
-            } else {
-                itemMetaCopy.lore(infoLore)
-            }
-
-            item.itemMeta = itemMetaCopy
-
-            val button = SGButton(
-                item
-            ).withListener { event: InventoryClickEvent ->
-                event.whoClicked.sendMessage("Order Deleted!)")
-                gui.removeButton((index + 10) + (gui.currentPage * 45))
-                Database.orderDao.delete(order)
-                gui.refreshInventory(actor.player)
-            }
-            val baseSlot = (index + 10) + (gui.currentPage * 45)
-
-            var slot = baseSlot
-
-            while (gui.getButton(slot) != null) {
-                if (slot >= 44) {
-                    throw IndexOutOfBoundsException("No more slots available in page ${gui.currentPage}")
-                }
-                slot++
-            }
-
-            gui.setButton( slot , button)
-
-
-        }
-        gui.refreshInventory(actor.player)
-
+        MyOrders.openMyOrders(actor)
     }
 }
