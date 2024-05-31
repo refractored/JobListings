756c0543ffaa362c0caf865825a634ce68f4ad20
David <me@refractored.net>
2024-05-31 10:30:59 -0500

    refactor: optimize imports

--- src/main/java/net/refractored/joblistings/commands/GetOrders.kt
+++ src/main/java/net/refractored/joblistings/commands/GetOrders.kt
@@ -1,24 +1,10 @@
 package net.refractored.joblistings.commands
 
-import com.samjakob.spigui.buttons.SGButton
-import com.samjakob.spigui.item.ItemBuilder
-import com.samjakob.spigui.menu.SGMenu
-import net.refractored.joblistings.JobListings.Companion.spiGUI
-import net.refractored.joblistings.database.Database
 import net.refractored.joblistings.gui.AllOrders
-import net.refractored.joblistings.order.Order
-import net.refractored.joblistings.serializers.ItemstackSerializers
-import net.refractored.joblistings.util.MessageUtil
-import org.bukkit.Bukkit
-import org.bukkit.Material
-import org.bukkit.event.inventory.InventoryClickEvent
 import revxrsal.commands.annotation.Command
 import revxrsal.commands.annotation.Description
-import revxrsal.commands.annotation.Optional
 import revxrsal.commands.bukkit.BukkitCommandActor
 import revxrsal.commands.bukkit.annotation.CommandPermission
-import revxrsal.commands.bukkit.player
-import kotlin.math.ceil
 
 
 class GetOrders {
--- src/main/java/net/refractored/joblistings/commands/ViewOrder.kt
+++ src/main/java/net/refractored/joblistings/commands/ViewOrder.kt
@@ -1,23 +1,10 @@
 package net.refractored.joblistings.commands
 
-import com.samjakob.spigui.buttons.SGButton
-import com.samjakob.spigui.item.ItemBuilder
-import com.samjakob.spigui.menu.SGMenu
-import net.refractored.joblistings.database.Database
-import net.refractored.joblistings.serializers.ItemstackSerializers
-import org.bukkit.event.inventory.InventoryClickEvent
+import net.refractored.joblistings.gui.MyOrders
 import revxrsal.commands.annotation.Command
 import revxrsal.commands.annotation.Description
 import revxrsal.commands.bukkit.BukkitCommandActor
 import revxrsal.commands.bukkit.annotation.CommandPermission
-import revxrsal.commands.bukkit.player
-import net.refractored.joblistings.JobListings.Companion.spiGUI
-import net.refractored.joblistings.gui.MyOrders
-import net.refractored.joblistings.order.Order
-import net.refractored.joblistings.util.MessageUtil
-import org.bukkit.Bukkit
-import org.bukkit.Material
-import kotlin.math.ceil
 
 
 class ViewOrder {
