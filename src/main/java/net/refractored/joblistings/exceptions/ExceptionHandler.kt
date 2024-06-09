5548ddb07a9c4ebef765e41abc5db48f20b8ea3c
David <me@refractored.net>
2024-06-09 13:58:07 -0500

    feat: command exception handler

---
+++ src/main/java/net/refractored/joblistings/exceptions/ExceptionHandler.kt
@@ -0,0 +1,66 @@
+package net.refractored.joblistings.exceptions
+
+import net.refractored.joblistings.util.MessageReplacement
+import net.refractored.joblistings.util.MessageUtil
+import revxrsal.commands.bukkit.sender
+import revxrsal.commands.command.CommandActor
+import revxrsal.commands.exception.*
+
+
+// This is for only the commands :3
+class ExceptionHandler : DefaultExceptionHandler() {
+    override fun invalidNumber(actor: CommandActor, exception: InvalidNumberException) {
+        actor.error("Wrong number input :(")
+    }
+
+    override fun commandInvocation(actor: CommandActor, exception: CommandInvocationException) {
+        MessageUtil.getMessage("General.UnexpectedError")
+        exception.cause.printStackTrace()
+    }
+
+    override fun tooManyArguments(actor: CommandActor, exception: TooManyArgumentsException) {
+        val command = exception.command
+        val usage = (command.path.toRealString() + " " + command.usage).trim { it <= ' ' }
+        actor.sender.sendMessage(
+            MessageUtil.getMessage(
+                "General.TooManyArguments",
+                listOf(
+                    MessageReplacement(usage)
+                )
+            )
+        )
+    }
+
+    override fun invalidCommand(actor: CommandActor, exception: InvalidCommandException) {
+        actor.sender.sendMessage(
+            MessageUtil.getMessage(
+                "General.InvalidCommand",
+                listOf(
+                    MessageReplacement(exception.input)
+                )
+            )
+        )    }
+
+    override fun noSubcommandSpecified(actor: CommandActor, exception: NoSubcommandSpecifiedException) {
+        actor.sender.sendMessage(
+            MessageUtil.getMessage("General.NoSubcommandSpecified")
+        )
+    }
+
+    override fun missingArgument(actor: CommandActor, exception: MissingArgumentException) {
+        actor.sender.sendMessage(
+            MessageUtil.getMessage(
+                "General.MissingArguments",
+                listOf(
+                    MessageReplacement(exception.parameter.name)
+                )
+            )
+        )
+    }
+
+    override fun noPermission(actor: CommandActor, exception: NoPermissionException) {
+        actor.sender.sendMessage(
+            MessageUtil.getMessage("General.NoPermission")
+        )
+    }
+}
