54fc497d973e0966c857099f46a0966949068f5a
refractored <me@refractored.net>
2024-06-09 18:09:56 -0500

    feat(HelpCmd): Messageify

--- src/main/java/net/refractored/joblistings/commands/HelpCommand.kt
+++ src/main/java/net/refractored/joblistings/commands/HelpCommand.kt
@@ -12,22 +12,6 @@ class HelpCommand {
     @Description("Displays command info")
     @DefaultFor("joblistings")
     fun help(actor: BukkitCommandActor) {
-        actor.reply( Component.text()
-            .append(MessageUtil.toComponent("<bold><gradient:#7ddb6d:#4CB13B>JobListings</gradient></bold><white>"))
-            .appendNewline()
-            .append(MessageUtil.toComponent("<gray>/joblistings orders<white>: View all pending orders"))
-            .appendNewline()
-            .append(MessageUtil.toComponent("<gray>/joblistings owned<white>: View and manage orders you own"))
-            .appendNewline()
-            .append(MessageUtil.toComponent("<gray>/joblistings claimed<white>: View the orders you claimed"))
-            .appendNewline()
-            .append(MessageUtil.toComponent("<gray>/joblistings complete<white>: Scans your inventory for items to complete an order"))
-            .appendNewline()
-            .append(MessageUtil.toComponent("<gray>/joblistings create hand<white>: Create an order from the item in your hand"))
-            .appendNewline()
-            .append(MessageUtil.toComponent("<gray>/joblistings create material<white>: Create an order from the specified material"))
-            .appendNewline()
-            .append(MessageUtil.toComponent("<gray>/joblistings<white>: Displays this menu"))
-        )
+        actor.reply(MessageUtil.getMessage("Help.Page1"))
     }
 }
