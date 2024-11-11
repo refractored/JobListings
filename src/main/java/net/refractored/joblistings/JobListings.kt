4007c5f96a1140efa98898e133f9fca6e067869a
refractored <me@refractored.net>
2024-11-11 14:20:00 -0600

    fix: Update paper message

--- src/main/java/net/refractored/joblistings/JobListings.kt
+++ src/main/java/net/refractored/joblistings/JobListings.kt
@@ -93,9 +93,14 @@ class JobListings : JavaPlugin() {
 
     override fun onEnable() {
         if (!PaperLib.isPaper()) {
+            logger.severe("-----------------------------------")
             logger.severe("This plugin requires Paper to run!")
+            logger.severe("Paper is a high-performance fork of Spigot.")
+            logger.severe("Everything that works on Spigot works on Paper.")
             logger.severe("Learn more here: https://papermc.io/")
+            logger.severe("")
             logger.severe("The plugin will now disable itself.")
+            logger.severe("-----------------------------------")
             throw IllegalStateException("Server is not running Paper.")
         }
 
